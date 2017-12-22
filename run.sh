14df1b26a1eb687c7a47eaf5c88433ded680a16e
Dalhatu Njidda <kulgee001@yahoo.com>
2017-12-22 13:58:48 +0000

    bug fixes

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 bash build.sh
-docker run -d -p 27017:27017 -p 28017:28017 --name mongodb mongodb mongod --httpinterface
-docker run -d --link mongodb:mongodb -it -p 8181:8080 --name app app npm start
+docker run -d -v ./mongodb/data/db:/data/db -p 27017:27017 -p 28017:28017 -name=mongodb mongodb mongod --httpinterface
+docker run –it -d --link=mongodb:mongodb -p 8080:3000 –name=userManager –v ./app:/usr/src/app –w /usr/src/app app npm start
