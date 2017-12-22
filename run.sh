ac400a202cc170b7a5c4238d9fc41bfb2383a7e4
Dalhatu Njidda <kulgee001@yahoo.com>
2017-12-22 16:12:19 +0000

    final deploy

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 bash build.sh
-docker run -d -v ./mongodb/data/db:/data/db -p 27017:27017 -p 28017:28017 --name mongodb mongodb mongod --httpinterface
-docker run –it -d -p 3000:3000 --name usermanager app
+docker run -d -v `pwd`/mongodb/data/db:/data/db -p 27017:27017 -p 28017:28017 --name mongodb mongodb mongod --httpinterface
+docker run -it -d -p 3000:3000 --link mongodb:mongodb --name usermanager app
